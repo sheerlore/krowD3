6e71e849b539254f4aa32afd27a1c5646227ce71
uai <noema01@outlook.jp>
2022-08-22 14:51:38 +0900

    github.io 用にファイルパス変更

--- src/World/components/rabbit/rabbit.js
+++ src/World/components/rabbit/rabbit.js
@@ -5,7 +5,9 @@ import { getRandomInt } from "../../../utils/getRandomInt.js";
 
 async function loadRabbit() {
   let loader = new GLTFLoader();
-  let rabbitData = await loader.loadAsync("/rmodel.glb");
+  let rabbitData = await loader.loadAsync(
+    "https://sheerlore.github.io/krowD3/assets/models/rmodel.glb"
+  );
   let rabbit = setupModel(rabbitData);
   rabbit.position.set(getRandomInt(-500, 500), 5, getRandomInt(-500, 500));
   return rabbit;
